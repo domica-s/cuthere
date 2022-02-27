645b97bc09e5977e17104c56458a7d2488390f7e
Domica <1155130940@link.cuhk.edu.hk>
2022-02-28 01:29:58 +0800

    style login and signup page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ mongoose.connect(params.DATABASECONNECTION);
 setuppassport();
 
 app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
