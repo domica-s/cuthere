6fe408f1b566e5c24cf6cab1a9f05d8d1ca50594
Domica <1155130940@link.cuhk.edu.hk>
2022-02-27 22:21:50 +0800

    add dependencies for authentication

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var path = require("path");
+var mongoose = require("mongoose");
+var cookieParser = require("cookie-parser");
+var passport = require("passport");
+var session = require("express-session");
+var flash = require("connect-flash");
 
 var app = express();
 
--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,16 @@ var express = require("express");
 
 var router = express.Router();
 
-
-router.get("/", function(req, res) {
-    res.render("home/");
-});
+router.get("/", (req, res) =>
+    res.render("home/index"));
  
-router.get("/home", function(req,res){
-    res.render("home/home");
-});
+router.get("/home", (req,res) =>
+    res.render("home/home"));
+
+router.get("/login", (req,res) =>
+    res.render("home/login"));
+
+router.get("/signup", (req,res) =>
+    res.render("home/signup"));
 
 module.exports = router;
