e0d7c379c111320c5cbe8a2d137afb951ec4c27b
domica-s <domica@link.cuhk.edu.hk>
2022-03-19 04:06:08 +0800

    add react-bootstrap, add navbar

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,18 +3,18 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import {Home, About} from './home/homePage'
 import {Login} from './home/loginPage'
 import {Event} from './home/eventPage'
-
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
       <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/event">Events</Link>
-        <hr/>
+          {/* <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/event">Events</Link>
+          <hr/> */}
+        <NavigationBar />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path='/about' element={<About/>} />
@@ -23,6 +23,31 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>    
+  );
+}
+
+
+function NavigationBar() {
+  return (
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="/" Link to="/home">
+        {/* <img
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '} */}
+      CUthere
+      </Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="/about" Link to="/about">About</Nav.Link>
+        <Nav.Link href="/login" Link to="/login">Login</Nav.Link>
+        <Nav.Link href="/event" Link to="/event">Events</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
   );
 }
 
