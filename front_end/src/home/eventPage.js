5d7765c83429addff490acf0ac10eda36c48214b
ethanlee928 <ethan2000.el@gmail.com>
2022-03-20 01:12:55 +0800

    front end could view all events after login in event page

--- front_end/src/home/eventPage.js
+++ front_end/src/home/eventPage.js
@@ -1,6 +1,32 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
 
 const API = '../../allevents'
+
+class OneEvent extends React.Component {
+
+    render() {
+        let data = this.props.data[1];
+        console.log(data[1]);
+        return (
+            <Container>
+                <Row className="justify-content-sm-center">
+                    <h3>{data.title}</h3>
+                    <br/>
+                    <p>Location: {data.venue}</p>
+                    <br/>
+                    <p>Date: {data.date}</p>
+                    <br/>
+                    <p>No. of participants: {data.numberOfParticipants} Quota: {data.quota}</p>
+                    <br/> 
+                    <p>Category: {data.activityCategory}</p>
+                </Row>
+                <hr/>
+            </Container>
+        );
+    }
+}
 
 class Event extends React.Component {
    
@@ -15,19 +41,18 @@ class Event extends React.Component {
         fetch(API)
             .then(res => res.json())
             .then(data => {
-                console.log('Received events: ', data);
-                this.setState( {events: data} )
+                console.log('Received events: ', Object.entries(data));
+                this.setState( {events: Object.entries(data)} )
             });
     }
 
     render() {
         let e = this.state.events
         return(
-            
-            <div className="container">
+            <Container>
                 <h2>Here are the events</h2>
-                <p>The events could be seen in reactDOM state</p>
-            </div>
+                {e.length > 0 && e.map((data, index) => <OneEvent data={data}/>)}
+            </Container>
         );
     }
 }
