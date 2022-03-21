fd6a9e2da9ae944f01666ab4d7e8755753577245
bryanwilsonkheng <bryanwilsonkheng@gmail.com>
2022-03-21 16:46:07 +0800

    make event item layout

--- front_end/src/home/eventPage.js
+++ front_end/src/home/eventPage.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
+import "./eventPage.css";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 
 const API = '../../allevents'
 
@@ -50,9 +54,57 @@ class Event extends React.Component {
         let e = this.state.events
         return(
             <Container>
+                <EventCard />
                 <h2>Here are the events</h2>
                 {e.length > 0 && e.map((data, index) => <OneEvent data={data}/>)}
             </Container>
+        );
+    }
+}
+
+class EventCard extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+          <Card
+            style={{width: "12rem", height: "17.5rem",display: "block",overflow: "hidden",whiteSpace: "nowrap",
+            }}
+          >
+            <Card.Img variant="top" src={require("./basket.jpeg")} />
+            <Card.Body>
+              <Card.Title
+                style={{textAlign: "left",width: "10rem",textOverflow: "ellipsis",overflow: "hidden",}}
+              >
+                CUHK ID
+              </Card.Title>
+              <div style={{ height: "2rem" }} className="row">
+                <div
+                  className="column text">
+                  <p style ={{textOverflow: "ellipsis",overflow: "hidden",textAlign: "left"}}>New Asia</p>
+                </div>
+                <div className="column text" style={{textAlign: "right",}}>
+                  09.00 21/02
+                </div>
+              </div>
+              <h6 style={{ textAlign: "left" }}>
+                {" "}
+                <Badge bg="secondary">Basketball</Badge>
+              </h6>
+              <p style={{ textAlign: "left", fontSize: "15px" }}>
+                Quota:{" "}
+                <Badge pill bg="dark">
+                  8/111
+                </Badge>
+                <span style={{ float: "right" }}>
+                  <Button variant="primary" size="sm">
+                    Join
+                  </Button>
+                </span>
+              </p>
+            </Card.Body>
+          </Card>
         );
     }
 }
