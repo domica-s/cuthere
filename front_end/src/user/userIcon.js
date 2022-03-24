c37052711912412cb7630239a15fb33443d91c53
ethanlee928 <ethan2000.el@gmail.com>
2022-03-24 13:09:00 +0800

    adde user/ for user profile development

---
+++ front_end/src/user/userIcon.js
@@ -0,0 +1,11 @@
+import React from "react";
+import demo from '../../public/userProfile.png';
+
+function Icon() {
+    return (
+        <img className="thumbnail-image" src={demo} alt="user pic" />
+    )
+}
+
+
+export {Icon}
