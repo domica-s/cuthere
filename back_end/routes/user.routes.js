521cfe8b70e5de75f60a26cbe688832cfa3229fb
bryanwilsonkheng <bryanwilsonkheng@gmail.com>
2022-04-03 02:26:40 +0800

    backend follow/unfollow

--- back_end/routes/user.routes.js
+++ back_end/routes/user.routes.js
@@ -8,5 +8,7 @@ var router = express.Router();
 router.get("/:sid", [authJwt.verifyToken], controller.getUserProfile);
 router.post("/:sid/comment", [authJwt.verifyToken], controller.leaveUserRating);
 router.post("/:sid/comment/update", [authJwt.verifyToken], controller.updateUserRating);
+router.post("/follow/:sid", [authJwt.verifyToken], controller.followUser);
+router.post("/unfollow/:sid", [authJwt.verifyToken], controller.unfollowUser);
 
 module.exports = router;
