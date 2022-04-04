e647ec08c1bba57defcae517d70b4a975b54b9f5
ethanlee928 <ethan2000.el@gmail.com>
2022-04-04 22:59:48 +0800

    added eventpic upload in event details

--- front_end/src/event/eventDetailsComponent/EventImage.jsx
+++ front_end/src/event/eventDetailsComponent/EventImage.jsx
@@ -1,28 +1,72 @@
 import React, { useEffect, useState } from 'react';
-import { FaRegCaretSquareDown } from 'react-icons/fa';
-import ImageGallery from 'react-image-gallery';
+import {Form, Button} from 'react-bootstrap';
+import defaultImage from '../../images/cuth.png';
+import authService from '../../services/auth.service';
+var params = require("../../params/params");
+
+
+const API = params.baseBackURL + "/file/upload";
+const API_Query = params.baseBackURL + "/file/";
+const API_DELETE = params.baseBackURL + "/file/delete";
+const currentUser = authService.getCurrentUser();
+
+const INITIAL_STATE = {
+    file: ""
+};
 
 function EventImage(props) {
-    const [Images, setImages] = useState([]);
+    console.log(props.eventID)
+    const [Images, setImages] = useState(INITIAL_STATE);
 
-    useEffect(()=> {
-        if (props.detail.images && props.detail.images.length > 0){
-            let images = [] ;
+    const onChangeFile = async(e) => {
+        setImages({
+            file: e.target.files[0]
+        });
+    }
 
-            props.detail.images && props.detail.images.map(item => {
-                images.push({
-                    original: `http://localhost:8080/${item}`,
-                    thumbnail: `http://localhost:8080/${item}`
-                })
-            })
-            setImages(images)
+    const onUploadFile = async(e) => {
+        e.preventDefault();
+        let data = new FormData();
+        data.append("file", Images.file, "event-" + props.eventID);
+        const uploadResult = await fetch(API, {
+            method: "POST",
+            headers: new Headers({
+                "x-access-token": currentUser.accessToken,
+                }),
+              body: data              
+        });
+
+        const resultJson = await uploadResult.json();
+        await window.alert(resultJson.message);
+    }
+
+    const onLoadPic = async(e) => {
+        const img = document.querySelector("#event-pic");
+        let api = API_Query + "event-" + props.eventID;
+        const loadResult = await fetch(api, {
+            method: "GET",
+            headers: new Headers({
+                "x-access-token": currentUser.accessToken,
+              }),            
+        })
+        const resultStatus = await loadResult.clone().status
+        const resultBlob = await loadResult.blob();
+        if (resultStatus == 200){
+            img.crossOrigin = 'anonymous';
+            img.src = await URL.createObjectURL(resultBlob);
         }
-    }, [props.detail])
-
+    }
 
   return (
       <div> 
-          <ImageGallery items = {Images} />
+          <img id="event-pic" src={defaultImage} alt="" className="center ui-w-80" onLoad={onLoadPic}/>
+          <br/>
+          <label className="m-3 btn btn-outline-primary">
+                Select new photo
+                <input type="file" className="account-settings-fileinput" onChange={onChangeFile}/>
+          </label> &nbsp;          
+          {Images.file && <Button className="m-3" variant="outline-warning" type="button" onClick={onUploadFile}>Upload</Button>}
+          {Images.file && <p>File selected: {Images.file.name}</p>}
       </div>
   )
 }
