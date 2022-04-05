c963d6890cc68008a7737d01828436ddca60ca4e
Domica <1155130940@link.cuhk.edu.hk>
2022-04-06 01:48:45 +0800

    make navbar resposnive

---
+++ front_end/src/FooterStyle.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+    background-color: #000;
+    display: flex;
+    color: #282c34;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    padding: 10px 22px;
+    justify-content: center;
+`
