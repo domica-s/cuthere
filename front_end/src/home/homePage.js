e94cc34787ec84683338c622d5bbb9ebd63c5051
ethanlee928 <ethan2000.el@gmail.com>
2022-04-09 01:27:29 +0800

    added aboutPage

--- front_end/src/home/homePage.js
+++ front_end/src/home/homePage.js
@@ -48,14 +48,4 @@ class Home extends React.Component {
     }
 }
 
-class About extends React.Component {
-    render() {
-        return (
-            <div className="container"> 
-                About Page 
-            </div>
-        )
-    }
-}
-
-export {Home, About}
+export {Home};
