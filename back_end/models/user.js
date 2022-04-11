db9f46639af2e693543fed2368df6cc10a0ac096
Domica <1155130940@link.cuhk.edu.hk>
2022-04-11 17:02:28 +0800

    remove user dependencies issue

--- back_end/models/user.js
+++ back_end/models/user.js
@@ -41,22 +41,111 @@ var userSchema = mongoose.Schema({
         content: {type: String},
         reviewAt: {type: Date, default:Date.now},
     }],
-    profilesInteracted:[{
-        user: {type: Number, ref: 'User'},
-    }],
     createdAt:{type:Date, default:Date.now},
     name:{type:String},
     country:{type:String},
 });
 
-// userSchema.post("remove", function(next){
+userSchema.pre('remove', function(res){
+    var User = require("./user");
+    var Event = require("./event");
+    // Remove all the assignment docs that reference the removed event 
+    // console.log(this);
+    // remove user dependencies
+    let update_review = {
+        $pull: { reviewHistory: { user: this.sid } },
+    }
 
-//     console.log("Hook called");
+    let update_followers = {
+        $pull: { followers: this._id}
+    }
+
+    let update_following = {
+        $pull: {following: this._id}
+    }
+
+    User.updateMany({}, update_review, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed from registered event");
+        }
+    })
+
+    User.updateMany({}, update_followers, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed this from other user's follower list");
+        }
+    })
+
+    User.updateMany({}, update_following, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed this from other user's following list");
+        }
+    })
 
 
+    // remove event dependencies
+    let update_comments = {
+        $pull: { chatHistory: { user: this.sid } },
+    }
 
-//     next();
-// })
+    let update_pinned = {
+        $pull: { pinnedComment: { user: this.sid } },
+    }
+
+    let update_participants = {
+        $pull: { participants: this.sid },
+    }
+
+    let update_no_of_participants = {
+        $inc: { numberOfParticipants: -1 },
+    }
+
+    Event.updateMany({}, update_comments, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed this user's comments from events");
+        }
+    })
+
+    Event.updateMany({}, update_pinned, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed this user's pinned comments from events");
+        }
+    })
+
+    Event.updateMany({}, update_participants, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed this user's pinned comments from participants lists");
+        }
+    })
+
+    Event.updateMany({}, update_no_of_participants, (err, result) => {
+        if (err) {
+            console.log("mongoDB error: " + err);
+        }
+        else {
+            // console.log("Successfully removed this user's pinned comments from participants count");
+        }
+    })
+
+});
 
 var User = mongoose.model("User", userSchema);
 
