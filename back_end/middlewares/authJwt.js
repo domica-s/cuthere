acf9e1567cc4dfae8d4ee2187135e67b19cd59ed
philiptl0906 <philiptl0906@gmail.com>
2022-05-05 02:00:55 +0800

    authJwt.js

--- back_end/middlewares/authJwt.js
+++ back_end/middlewares/authJwt.js
@@ -1,8 +1,18 @@
+// The codes are used for middlewares for authentication
+// PROGRAMMER: Pierson and Domica
+// This js file is to handle the middleware for authentication
+// Revised on 5/5/2022
+
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 const User = require("../models/user");
 
 verifyToken = (req, res, next) => {
+                /*
+      This function is used to verify the validity of the token
+      Requirements (header): pass x-access token into the header 
+      */
+
   let token = req.headers["x-access-token"];
   if (!token) {
     return res.status(403).send({ message: "No token provided!" });
@@ -17,6 +27,10 @@ verifyToken = (req, res, next) => {
 };
 
 isAdmin = (req, res, next) => {
+                  /*
+      This function is used to check if the specific user is admin or not
+      */
+     
   User.findById(req.userId).exec((err, user) => {
     if (err) {
       res.status(500).send({ message: err });
